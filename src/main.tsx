e325ce1d8a09061e195e6cc25e53b54565e302f3
Fsk8 <montealegrefavio@gmail.com>
2026-04-13 23:54:40 -0400

    fix import privywrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { PrivyProviderWrapper } from "@/components/PrivyProviderWrapper";
+import { PrivyProviderWrapper } from "@/components/Privyproviderwrapper";
 import App from "./App";
 import "./index.css";
 
